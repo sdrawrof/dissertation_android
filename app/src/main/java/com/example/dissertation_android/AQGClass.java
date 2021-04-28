d69bef5f8962827fd376ece0e5df3c1f30a9f881
sdrawrof <a.macheny@outlook.com>
2021-04-28 19:59:02 +0100

    It did! woooow but gitignore was ignored lol

--- app/src/main/java/com/example/dissertation_android/AQGClass.java
+++ app/src/main/java/com/example/dissertation_android/AQGClass.java
@@ -2,4 +2,5 @@ package com.example.dissertation_android;
 
 public class AQGClass {
 
+    //convert java files to here - how in the world am I gonna apply them to the imported files and show loading and stuff lol
 }
